b688bf78c30ba50af01361166db4340eecd302d0
Craig Fowler <craig@wearegiants.com>
2015-11-05 19:11:21 -0800

    things

--- parts/footer.php
+++ parts/footer.php
@@ -1,11 +1,10 @@
 <footer id="footer">
    <div class="fs-row">
-      <nav id="footer--nav_center" class="fs-cell fs-lg-12 fs-md-6 fs-sm-hide fs-centered text-center">
-         <a href="?page=search" class="btn btn-nav">Wedding Details</a>
-         <a href="?page=search" class="btn btn-nav">The Venue</a>
-         <a href="?page=search" class="btn btn-nav">The After Party</a>
-         <a href="?page=search" class="btn btn-nav">Registry</a>
-         <a href="#" class="btn btn-nav">us@catherineandmax.com</a>
+      <nav id="footer--nav_center" class="fs-cell fs-lg-12 fs-md-6 fs-sm-3 fs-centered text-center">
+         <a href="#" class="btn btn-nav">Home</a>
+         <a href="#basics" class="btn btn-nav">The Basics</a>
+         <a href="#registry" class="btn btn-nav">Registered Here</a>
+         <a href="mailto:us@catherineandmax.com" class="btn btn-nav">us@catherineandmax.com</a>
       </nav>
    </div>
 </footer>
